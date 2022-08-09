3d3dc9e9ebad438d634c0c389b68b17f4e550cd6
Cees van de Griend <cees.van.de.griend@xantara-it.nl>
2022-08-09 11:18:58 +0200

    Added comments to access_key.sh

--- access_key.sh
+++ access_key.sh
@@ -1,5 +1,10 @@
+#
+# Save access key in an environment variable for Terraform remote state.
+#
+# $ az storage account list --resource-group xantara-it-rg --output table
+#
 
-RESOURCE_GROUP_NAME=rg-infra
-STORAGE_ACCOUNT_NAME=xantaraitggf4ga07
+RESOURCE_GROUP_NAME=xantara-it-rg
+STORAGE_ACCOUNT_NAME=xantaraitiw1oy9f6
 
-export ARM_ACCESS_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+export ARM_ACCESS_KEY=$(az storage account keys list --resource-group ${RESOURCE_GROUP_NAME} --account-name ${STORAGE_ACCOUNT_NAME} --query '[0].value' -o tsv)
